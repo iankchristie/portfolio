061f06539c0f33e8d369f8005f9bd3e311a1e474
Ian Christie <iankchristie@gmail.com>
2025-11-06 08:17:15 -0700

    fix footer.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p className="copyright">© Ian Christie | 2025</p>
         <nav className="footer-nav">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#work">Work</a>
-          <a href="#blog">Blog</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
         </nav>
       </div>
     </footer>
